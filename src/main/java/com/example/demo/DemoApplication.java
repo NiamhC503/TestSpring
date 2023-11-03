9fa5901284681bd79661928bf0ee117360c14124
NiamhC503 <n.connaghan1@universityofgalway.ie>
2023-11-03 16:21:38 +0000

    Commit 3 (Jenkinsfile archiving)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,21 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
+	@RequestMapping("/")
+	public String hello1(){
+		return "Hello";
+	}
+
+	@RequestMapping("/hello")
+	public String hello2(){
+		return "Hello Again!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
